fea5d015634bd4222a7bda5ec2284123d2230478
gyoritamas <gyoritamasnoe@gmail.com>
2020-12-22 09:42:29 +0100

    Add project files.

---
+++ Lottoszamok/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottoszamok
+{
+    class Program
+    {
+        private static void ReadFile(String file)
+        {
+            using (StreamReader sr = new StreamReader(file))
+            {
+                sr.ReadLine();      // skip header
+                HashSet<DateTime> datumok = new HashSet<DateTime>();
+                while (!sr.EndOfStream)
+                {
+                    string[] lines = sr.ReadLine().Split(';');
+                    datumok.Add(Convert.ToDateTime(lines[0]));
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            #region 1.feladat
+            Console.WriteLine("1. feladat:");
+            String filePath = @"..\..\src\sorsolas.csv";
+            ReadFile(filePath);
+            #endregion
+        }
+    }
+}
---
+++ Lottoszamok/Sorsolas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottoszamok
+{
+    class Sorsolas
+    {
+        private DateTime sorsolasNapja;
+        public int MyProperty { get; set; }
+        public Sorsolas(DateTime datum)
+        {
+            this.sorsolasNapja = datum;
+        }
+
+    }
+}
